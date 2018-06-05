55f09180cd855306c15b63d76bfa1c7662cfc0d8
Kalle Klimkewitz <mucklka@kochgroup.org>
2018-06-05 16:51:14 +0200

    [mongoplyr] Updates and Auto Re-Indent

--- R/mongo.R
+++ R/mongo.R
@@ -4,196 +4,207 @@
 NULL
 
 mongoPrimitiveOps = c("&"
-	, "|"
-	, "=="
-	, "!="
-	, ">"
-	, ">="
-	, "<"
-	, "<="
-	, "%in%"
-	, "+"
-	, "-"
-	, "/"
-	, "*"
+                      , "|"
+                      , "=="
+                      , "!="
+                      , ">"
+                      , ">="
+                      , "<"
+                      , "<="
+                      , "%in%"
+                      , "+"
+                      , "-"
+                      , "/"
+                      , "*"
 )
 
 exprParser <- function()
 {
-	callingFrame <- parent.frame(2)
-
-	function(expr, dollarForMemberAccess = F)
-	{
-		visit <- function(x)
-		{
-			if (is.atomic(x))
-			{
-				return(list(type = "atomic", value = x))
-			}
-			else if (is.name(x))
-			{
-				value = deparse(x)
-
-				# period prefix indicates a mongo field (some contexts require the $ prefix)
-				if (startsWith(value, "."))
-				{
-					value <- sub("^\\.", ifelse(dollarForMemberAccess, "$", ""), value)
-					return(list(type = "member", value = value))
-				}
-				# otherwise, it's an R object, so add that actual value to the ast
-				else
-				{
-					obj <- get(value, envir = callingFrame)
-					return(list(type = "atomic", value = obj))
-				}
-			}
-			else if (is.call(x))
-			{
-				op <- visit(x[[1]])
-				opText <- deparse(x[[1]])
-				argCount <- length(x) - 1
-
-				# make an exception for negated literals - they're actually calls in R
-				# resolve them ahead of time instead of shelling out to an invalid $subtract call
-				isPrimitiveException <- opText == "-" & argCount == 1 & is.numeric(x[[2]])
-
-				# if it's a primitive op, or we're just proxying to a mongo expression,
-				# then create an ast node to render that out later
-				if (!isPrimitiveException & (opText %in% mongoPrimitiveOps | startsWith(opText, ".")))
-				{
-					args <- lapply(x[2:length(x)], visit)
-					opText <- sub("^\\.", "", opText)
-					return(list(type = "call", op = opText, args = args))
-				}
-				# otherwise, it's a proper R function call (and so contains no mongo field/func refs)
-				# so resolve the return value pre-query and sub that in as an atomic ast node
-				else
-				{
-					rargs <- list()
-					if (argCount > 0)
-					{
-						rargs <- as.list(x[2:length(x)])
-					}
-
-					result <- do.call(op$value, rargs, envir = callingFrame)
-					return(list(type = "atomic", value = result))
-				}
-			}
-			else
-			{
-				stop(paste0("unhandled type: ", typeof(x))) # nocov
-			}
-		}
-
-		visit(expr)
-	}
+  callingFrame <- parent.frame(2)
+  
+  function(expr, dollarForMemberAccess = F)
+  {
+    visit <- function(x)
+    {
+      if (is.atomic(x))
+      {
+        return(list(type = "atomic", value = x))
+      }
+      else if (is.name(x))
+      {
+        value = deparse(x)
+        
+        # period prefix indicates a mongo field (some contexts require the $ prefix)
+        if (startsWith(value, "."))
+        {
+          value <- sub("^\\.", ifelse(dollarForMemberAccess, "$", ""), value)
+          return(list(type = "member", value = value))
+        }
+        # otherwise, it's an R object, so add that actual value to the ast
+        else
+        {
+          obj <- get(value, envir = callingFrame)
+          return(list(type = "atomic", value = obj))
+        }
+      }
+      else if (is.call(x))
+      {
+        op <- visit(x[[1]])
+        opText <- deparse(x[[1]])
+        argCount <- length(x) - 1
+        
+        # make an exception for negated literals - they're actually calls in R
+        # resolve them ahead of time instead of shelling out to an invalid $subtract call
+        isPrimitiveException <- opText == "-" & argCount == 1 & is.numeric(x[[2]])
+        
+        # if it's a primitive op, or we're just proxying to a mongo expression,
+        # then create an ast node to render that out later
+        if (!isPrimitiveException & (opText %in% mongoPrimitiveOps | startsWith(opText, ".")))
+        {
+          args <- lapply(x[2:length(x)], visit)
+          opText <- sub("^\\.", "", opText)
+          return(list(type = "call", op = opText, args = args))
+        }
+        # otherwise, it's a proper R function call (and so contains no mongo field/func refs)
+        # so resolve the return value pre-query and sub that in as an atomic ast node
+        else
+        {
+          rargs <- list()
+          if (argCount > 0)
+          {
+            rargs <- as.list(x[2:length(x)])
+          }
+          
+          result <- do.call(op$value, rargs, envir = callingFrame)
+          return(list(type = "atomic", value = result))
+        }
+      }
+      else
+      {
+        stop(paste0("unhandled type: ", typeof(x))) # nocov
+      }
+    }
+    
+    visit(expr)
+  }
 }
 
 
 mongoAstToList <- function(ast)
 {
-	visit <- function(node)
-	{
-		arrayExpr <- function(name, requiredArity = NA)
-		{
-			function(node)
-			{
-				ret <- list()
-				args <- lapply(node$args, visit)
-
-				if (!is.na(requiredArity) & length(args) != requiredArity)
-				{
-					stop(paste("Expected", requiredArity, "args for", name, "operator, got", length(args))) # nocov
-				}
-
-				ret[[paste0("$", name)]] <- args
-				ret
-			}
-		}
-
-		binaryExpr <- function(name, requiresArray = F)
-		{
-			function(node)
-			{
-				argList <- list()
-				rhs <- visit(node$args[[2]])
-
-				if (requiresArray & length(rhs) == 1)
-				{
-					rhs <- list(rhs)
-				}
-
-				argList[[paste0("$", name)]] <- rhs
-
-				ret <- list()
-				field <- visit(node$args[[1]])
-				ret[[field]] <- argList
-				ret
-			}
-		}
-
-		renderFunc <- function(node)
-		{
-			ret <- list()
-			ret[[paste0("$", node$op)]] <- visit(node$args[[1]])
-			ret
-		}
-
-		renderSubobject <- function(node)
-		{
-			lapply(node$args, visit)
-		}
-
-		if (node$type == "call")
-		{
-			func <- switch(node$op,
-
-				# expressions
-				"&" = { arrayExpr("and") },
-				"|" = { arrayExpr("or") },
-				"==" = { binaryExpr("eq") },
-				"!=" = { binaryExpr("ne") },
-				">" = { binaryExpr("gt") },
-				">=" = { binaryExpr("gte") },
-				"<" = { binaryExpr("lt") },
-				"<=" = { binaryExpr("lte") },
-				"+" = { arrayExpr("add") },
-				"-" = { arrayExpr("subtract", 2) },
-				"*" = { arrayExpr("multiply") },
-				"/" = { arrayExpr("divide", 2) },
-				"%in%" = { binaryExpr("in", T) },
-				"substr" = { arrayExpr("substr", 3) },
-
-				# accumulators
-				"sum" = { renderFunc },
-				"min" = { renderFunc },
-				"max" = { renderFunc },
-				"first" = { renderFunc },
-				"list" = { renderSubobject },
-
-				stop(paste("unhandled operator:", node$op))
-			)
-
-			func(node)
-		}
-		else if (node$type == "atomic" || node$type == "member")
-		{
-			node$value
-		}
-		else
-		{
-			stop(paste("unhandled type:", node$type)) # nocov
-		}
-	}
-
-	visit(ast)
+  visit <- function(node)
+  {
+    arrayExpr <- function(name, requiredArity = NA)
+    {
+      function(node)
+      {
+        ret <- list()
+        args <- lapply(node$args, visit)
+        
+        if (!is.na(requiredArity) & length(args) != requiredArity)
+        {
+          stop(paste("Expected", requiredArity, "args for", name, "operator, got", length(args))) # nocov
+        }
+        
+        ret[[paste0("$", name)]] <- args
+        ret
+      }
+    }
+    
+    binaryExpr <- function(name, requiresArray = F)
+    {
+      function(node)
+      {
+        argList <- list()
+        rhs <- visit(node$args[[2]])
+        
+        if (requiresArray & length(rhs) == 1)
+        {
+          rhs <- list(rhs)
+        }
+        
+        argList[[paste0("$", name)]] <- rhs
+        
+        ret <- list()
+        field <- visit(node$args[[1]])
+        ret[[field]] <- argList
+        ret
+      }
+    }
+    
+    renderFunc <- function(node)
+    {
+      ret <- list()
+      ret[[paste0("$", node$op)]] <- visit(node$args[[1]])
+      ret
+    }
+    
+    renderSubobject <- function(node)
+    {
+      lapply(node$args, visit)
+    }
+    
+    if (node$type == "call")
+    {
+      func <- switch(node$op,
+                     
+                     # expressions
+                     "&" = { arrayExpr("and") },
+                     "|" = { arrayExpr("or") },
+                     "==" = { binaryExpr("eq") },
+                     "!=" = { binaryExpr("ne") },
+                     ">" = { binaryExpr("gt") },
+                     ">=" = { binaryExpr("gte") },
+                     "<" = { binaryExpr("lt") },
+                     "<=" = { binaryExpr("lte") },
+                     "+" = { arrayExpr("add") },
+                     "-" = { arrayExpr("subtract", 2) },
+                     "*" = { arrayExpr("multiply") },
+                     "/" = { arrayExpr("divide", 2) },
+                     "%in%" = { binaryExpr("in", T) },
+                     "substr" = { arrayExpr("substr", 3) },
+                     
+                     # Project Functions
+                     "date" = { renderFunc },
+                     "oid" = {renderFunc},
+                     "year" = { renderFunc},
+                     "month" = {renderFunc},
+                     "dayOfMonth" = {renderFunc},
+                     "hour" = {renderFunc},
+                     "floor" = {renderFunc},
+                     "ceil" = {renderFunc},
+                     "ln" = {renderFunc},
+                     
+                     # accumulators
+                     "sum" = { renderFunc },
+                     "min" = { renderFunc },
+                     "max" = { renderFunc },
+                     "first" = { renderFunc },
+                     "list" = { renderSubobject },
+                     
+                     stop(paste("unhandled operator:", node$op))
+      )
+      
+      func(node)
+    }
+    else if (node$type == "atomic" || node$type == "member")
+    {
+      node$value
+    }
+    else
+    {
+      stop(paste("unhandled type:", node$type)) # nocov
+    }
+  }
+  
+  visit(ast)
 }
 
 createStage <- function(name, payload)
 {
-	ret <- list()
-	ret[[paste0("$", name)]] <- payload
-	jsonlite::toJSON(ret, auto_unbox = T)
+  ret <- list()
+  ret[[paste0("$", name)]] <- payload
+  jsonlite::toJSON(ret, auto_unbox = T)
 }
 
 #' Represents a MongoDB aggregation pipeline query.
@@ -212,14 +223,14 @@ setGeneric("mmatch", function(p, expr) standardGeneric("mmatch"))
 #' @rdname mmatch-methods
 #' @aliases mmatch,MongoPipeline-method
 setMethod("mmatch", signature(p = "MongoPipeline"),
-	function(p, expr)
-	{
-		parser <- exprParser()
-		ast <- parser(substitute(expr))
-		stage <- createStage("match", mongoAstToList(ast))
-		p@stages <- c(p@stages, stage)
-		p
-	})
+          function(p, expr)
+          {
+            parser <- exprParser()
+            ast <- parser(substitute(expr))
+            stage <- createStage("match", mongoAstToList(ast))
+            p@stages <- c(p@stages, stage)
+            p
+          })
 
 #' Add a $limit stage to a MongoPipeline.
 #' @param p A \linkS4class{MongoPipeline} instance.
@@ -232,11 +243,11 @@ setGeneric("mlimit", function(p, limit) standardGeneric("mlimit"))
 #' @rdname mlimit-methods
 #' @aliases mlimit,MongoPipeline,numeric-method
 setMethod("mlimit", signature(p = "MongoPipeline", limit = "numeric"),
-	function(p, limit)
-	{
-		p@stages <- c(p@stages, createStage("limit", jsonlite::unbox(limit)))
-		p
-	})
+          function(p, limit)
+          {
+            p@stages <- c(p@stages, createStage("limit", jsonlite::unbox(limit)))
+            p
+          })
 
 #' Add a $group stage to a MongoPipeline.
 #' @param p A \linkS4class{MongoPipeline} instance.
@@ -251,18 +262,18 @@ setGeneric("mgroup", function(p, by, ...) standardGeneric("mgroup"))
 #' @rdname mgroup-methods
 #' @aliases mgroup,MongoPipeline-method
 setMethod("mgroup", signature(p = "MongoPipeline"),
-	function(p, by, ...)
-	{
-		subbedArgs <- as.list(substitute(list(...)))[-1L]
-		subbedArgs[["_id"]] = substitute(by)
-
-		parser <- exprParser()
-		asts <- lapply(subbedArgs, function(a) { parser(a, dollarForMemberAccess = T) })
-		argList <- lapply(asts, mongoAstToList)
-
-		p@stages <- c(p@stages, createStage("group", argList))
-		p
-	})
+          function(p, by, ...)
+          {
+            subbedArgs <- as.list(substitute(list(...)))[-1L]
+            subbedArgs[["_id"]] = substitute(by)
+            
+            parser <- exprParser()
+            asts <- lapply(subbedArgs, function(a) { parser(a, dollarForMemberAccess = T) })
+            argList <- lapply(asts, mongoAstToList)
+            
+            p@stages <- c(p@stages, createStage("group", argList))
+            p
+          })
 
 #' Add an $unwind stage to a MongoPipeline.
 #' @param p A \linkS4class{MongoPipeline} instance.
@@ -275,13 +286,13 @@ setGeneric("munwind", function(p, expr) standardGeneric("munwind"))
 #' @rdname munwind-methods
 #' @aliases munwind,MongoPipeline-method
 setMethod("munwind", signature(p = "MongoPipeline"),
-	function(p, expr)
-	{
-		parser <- exprParser()
-		arg <- parser(substitute(expr), dollarForMemberAccess = T)
-		p@stages <- c(p@stages, createStage("unwind", mongoAstToList(arg)))
-		p
-	})
+          function(p, expr)
+          {
+            parser <- exprParser()
+            arg <- parser(substitute(expr), dollarForMemberAccess = T)
+            p@stages <- c(p@stages, createStage("unwind", mongoAstToList(arg)))
+            p
+          })
 
 #' Add a $sort stage to a MongoPipeline.
 #' @param p A \linkS4class{MongoPipeline} instance.
@@ -294,34 +305,35 @@ setGeneric("msort", function(p, ...) standardGeneric("msort"))
 #' @rdname msort-methods
 #' @aliases msort,MongoPipeline-method
 setMethod("msort", signature(p = "MongoPipeline"),
-	function(p, ...)
-	{
-		namedArgs <- lapply(list(...), jsonlite::unbox)
-		names(namedArgs) <- sapply(names(namedArgs), function(n) sub("^\\.", "", n))
-		p@stages <- c(p@stages, createStage("sort", namedArgs))
-		p
-	})
+          function(p, ...)
+          {
+            namedArgs <- lapply(list(...), jsonlite::unbox)
+            names(namedArgs) <- sapply(names(namedArgs), function(n) sub("^\\.", "", n))
+            p@stages <- c(p@stages, createStage("sort", namedArgs))
+            p
+          })
 
 
 #' Add a $project stage to a MongoPipeline.
 #' @param p A \linkS4class{MongoPipeline} instance.
-#' @param ... Named arguments for mproject fields, indicating whether to include (1) or exclude (0)
+#' @param ... Named arguments for mproject fields, indicating whether to include (1) or exclude (0), or functions of fields
 #' @return A copy of the previous pipeline, with the new stage added.
 #' @rdname mproject-methods
 #' @export
 setGeneric("mproject", function(p, ...) standardGeneric("mproject"))
 
-#' @rdname mproject-methods
-#' @aliases mproject,MongoPipeline-method
 setMethod("mproject", signature(p = "MongoPipeline"),
-					function(p, ...)
-					{
-						namedArgs <- lapply(list(...), jsonlite::unbox)
-						names(namedArgs) <- sapply(names(namedArgs), function(n) sub("^\\.", "", n))
-						p@stages <- c(p@stages, createStage("project", namedArgs))
-						p
-					})
-
+          function(p, ...)
+          {
+            subbedArgs <- as.list(substitute(list(...)))[-1L]
+            
+            parser <- exprParser()
+            asts <- lapply(subbedArgs, function(a) { parser(a, dollarForMemberAccess = T) })
+            argList <- lapply(asts, mongoAstToList)
+            
+            p@stages <- c(p@stages, createStage("project", argList))
+            p
+          })
 
 #' Execute a MongoPipeline's current query.
 #' @param p A \linkS4class{MongoPipeline} instance.
@@ -335,12 +347,12 @@ setGeneric("mexecute", function(p, client, verbose = FALSE) standardGeneric("mex
 #' @rdname mexecute-methods
 #' @aliases mexecute,MongoPipeline-method
 setMethod("mexecute", signature(p = "MongoPipeline"),
-					function(p, client, verbose = FALSE)
-					{
-						pipeline <- paste0("[", toString(p@stages), "]")
-						if(verbose) cat("Mongo query used:", pipeline, "\n")
-						frame <- client$aggregate(pipeline)
-						flattenedFrame <- jsonlite::flatten(frame)
-						colnames(flattenedFrame) <- gsub("^_id", "id", colnames(flattenedFrame))
-						flattenedFrame
-					})
+          function(p, client, verbose = FALSE)
+          {
+            pipeline <- paste0("[", toString(p@stages), "]")
+            if(verbose) cat("Mongo query used:", pipeline, "\n")
+            frame <- client$aggregate(pipeline)
+            flattenedFrame <- jsonlite::flatten(frame)
+            colnames(flattenedFrame) <- gsub("^_id", "id", colnames(flattenedFrame))
+            flattenedFrame
+          })
